c86c2a690c2dc619cdda6a3ac99036a3d7aff7a6
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2020-10-08 15:05:59 +0300

    Added coloring

--- 1/u1.js
+++ 1/u1.js
@@ -2,12 +2,13 @@
 var canvas;
 var ctx;
 var canvasEdgeSize = 500;
+var stepCount = 7;
 
 function drawFractal(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    draw(7);
+    draw(stepCount);
 }
 
 function draw(currentStep) {
@@ -16,41 +17,49 @@ function draw(currentStep) {
         drawBaseShape();
     } else {
         ctx.save();
-        transformF1();
+        transformF1(currentStep);
         draw(currentStep - 1)
         ctx.restore();
     
         ctx.save();
-        transformF2();
+        transformF2(currentStep);
         draw(currentStep - 1);
         ctx.restore();
     
         ctx.save();
-        transformF3();
+        transformF3(currentStep);
         draw(currentStep - 1);
         ctx.restore();
 
         ctx.save();
-        transformF4();
+        transformF4(currentStep);
         draw(currentStep - 1);
         ctx.restore();
     }
 
 }
 
-function transformF1(){
+function transformF1(currentStep){
+    if (currentStep == stepCount)
+        ctx.fillStyle = '#99ccff';
 	ctx.transform(0.5, 0, 0, 0.5, canvasEdgeSize / 2, 0);
 }
 
-function transformF2(){
+function transformF2(currentStep){
+    if (currentStep == stepCount)
+        ctx.fillStyle = '#66ffcc';
 	ctx.transform(-0.5, 0, 0, 0.5, canvasEdgeSize, canvasEdgeSize / 2);
 }
 
-function transformF3(){
+function transformF3(currentStep){
+    if (currentStep == stepCount)
+        ctx.fillStyle = '#ff3399';
 	ctx.transform(-0.5, 0, 0, -0.5, canvasEdgeSize / 2, canvasEdgeSize);
 }
 
-function transformF4(){
+function transformF4(currentStep){
+    if (currentStep == stepCount)
+        ctx.fillStyle = '#ff9900';
 	ctx.transform(0, 0.25, 0.25, 0, canvasEdgeSize / 4, canvasEdgeSize / 4);
 }
 
