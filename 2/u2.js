15528fce6ebfeb33258fd0be8bad48617e6d4c16
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2020-11-02 15:39:26 +0200

    WIP: staris with underlying structure

--- 2/u2.js
+++ 2/u2.js
@@ -12,6 +12,10 @@ function createPlane() {
     return groundPlane;
 }
 
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
 function createRenderer() {
     var renderer = new THREE.WebGLRenderer();
 
@@ -29,6 +33,7 @@ $(function() {
     var groundPlane = createPlane();
     scene.add(groundPlane);
 
+    //The X axis is red. The Y axis is green. The Z axis is blue.
     const axesHelper = new THREE.AxesHelper(20);
     scene.add( axesHelper );
 
@@ -44,11 +49,19 @@ $(function() {
     var boundingBoxWidth = 14;
 
 
-    var supportThicness = 0.5; 
+    var supportDepth = 0.5; 
+    var supportWidth = 1; 
     var spaceBetweenFootsteps = 2;
    
 
     var step, frame, box;
+    var currentXCoordinate = 0;
+    var currentZCoordinate = 0;
+
+    var stairRotationInDegrees = 45;
+
+    var verticalSupportRadius = 0.6;
+
     // stack of boxes
     for (i = 0; i < 5; i++) {
         var boxGeometry = new THREE.BoxGeometry(stepDepth,stepHeight,stepWidth);
@@ -56,21 +69,52 @@ $(function() {
         box = new THREE.Mesh(boxGeometry, boxMaterial);
 
         box.castShadow = true;
-        box.position.x  = boundingBoxDepth - stepDepth / 2;
+        // box.position.x  = boundingBoxDepth - stepDepth / 2;
         box.position.z = boundingBoxWidth - stepWidth / 2;
 
         var frameGeometry = new THREE.BoxGeometry(2 * boundingBoxDepth, stepHeight , 2 * boundingBoxWidth);
         var frameMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc, wireframe: true});
         frame = new THREE.Mesh(frameGeometry, frameMaterial);
 
+        var stepSupportMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
+
+        var horizontalStepSupportGeometry = new THREE.BoxGeometry(verticalSupportRadius + stepDepth, supportDepth, supportWidth);
+        var horizontalStepSupport = new THREE.Mesh(horizontalStepSupportGeometry, stepSupportMaterial);
+        horizontalStepSupport.position.x = verticalSupportRadius;
+        horizontalStepSupport.position.y = -(stepHeight / 2 + supportDepth / 2);
+        horizontalStepSupport.position.z = boundingBoxWidth - stepWidth / 2;
+
+        var verticalStepSupportGeometry = new THREE.CylinderGeometry(verticalSupportRadius, verticalSupportRadius, stepHeight * 2 + spaceBetweenFootsteps);
+        var verticalStepSupport = new THREE.Mesh(verticalStepSupportGeometry, stepSupportMaterial);
+        verticalStepSupport.position.x = stepDepth / 2 + verticalSupportRadius;
+        verticalStepSupport.position.y = supportDepth;
+        verticalStepSupport.position.z = boundingBoxWidth - stepWidth / 2;
+
         step = new THREE.Object3D();
         step.add(box);
         step.add(frame);
+        step.add(horizontalStepSupport);
+        step.add(verticalStepSupport);
 
-        step.position.y =  supportThicness + (stepHeight / 2) + i * (stepHeight + spaceBetweenFootsteps);
-        step.rotation.y = Math.atan(2.5 / 7) * i; // step rotation
+        step.position.x = currentXCoordinate;
+        step.position.z = currentZCoordinate;
+        step.position.y = supportDepth + (stepHeight / 2) + i * (stepHeight + spaceBetweenFootsteps);
+        step.rotation.y = degreesToRadians(stairRotationInDegrees) * i;//Math.atan((stepDepth / 2) / stepWidth) * i; // step rotation
 
         scene.add(step);
+
+        currentXCoordinate = currentXCoordinate -2.5 * Math.cos(-(degreesToRadians(stairRotationInDegrees)) * i);
+        currentZCoordinate = currentZCoordinate -2.5 * Math.sin(-(degreesToRadians(stairRotationInDegrees)) * i);
+
+        console.error('x', currentXCoordinate)
+        console.error('z',currentZCoordinate)
+
+
+        
+        
+    
+
+      
     }
   
     // position and point the camera to the center of the scene
