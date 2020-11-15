c65411f1b5ab5236084ab1690b17384ee7c273e0
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2020-11-15 14:27:34 +0200

    WIP rail support

--- 2/u2.js
+++ 2/u2.js
@@ -1,4 +1,4 @@
-function createPlane() {
+function setupPlane(scene) {
     var groundPlaneGeometry = new THREE.PlaneGeometry(50, 50);
     var groundPlaneMaterial =    new THREE.MeshLambertMaterial({color: 0xffffff});
     var groundPlane = new THREE.Mesh(groundPlaneGeometry, groundPlaneMaterial);
@@ -9,7 +9,7 @@ function createPlane() {
     groundPlane.position.y = 0;
     groundPlane.position.z = 0;
 
-    return groundPlane;
+    scene.add(groundPlane);
 }
 
 function degreesToRadians(degrees) {
@@ -26,40 +26,57 @@ function createRenderer() {
     return renderer;
 }
 
-$(function() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000); // vertical frustum, aspect ratio, near groundPlane, far groundPlane
-    var renderer = createRenderer();
-    var groundPlane = createPlane();
-    scene.add(groundPlane);
+function clearScene(scene) {
+    while(scene.children.length > 0){ 
+        scene.remove(scene.children[0]); 
+    }
+}
 
-    //The X axis is red. The Y axis is green. The Z axis is blue.
+function redraw(scene, stepCount, stepRotationInDegrees) {
+    clearScene(scene);
+    draw(scene, stepCount, stepRotationInDegrees);
+}
+
+function setupLight(scene) {
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set( -40, 60, -10 );
+    spotLight.castShadow = true;
+    scene.add( spotLight );
+
+    var ambientLight = new THREE.AmbientLight(0x404040);
+    scene.add(ambientLight);
+}
+
+function createHelperAxis(scene) {
+    // The X axis is red. The Y axis is green. The Z axis is blue.
     const axesHelper = new THREE.AxesHelper(20);
     scene.add( axesHelper );
+}
 
-    // create box
-    var stepDepth = 5;
-    var stepWidth = 10;
-    var stepHeight = 1;
+function draw(scene, stepCount, stepRotationInDegrees) {
+    const stepDepth = 5;
+    const stepWidth = 10;
+    const stepHeight = 1;
 
-    var supportDepth = 0.5; 
-    var supportWidth = 1; 
-    var spaceBetweenFootsteps = 2;
+    const supportDepth = 0.5; 
+    const supportWidth = 1; 
+    const verticalSupportRadius = 0.6;
+    const spaceBetweenFootsteps = 2;
+
+    var railHeight = 10;
    
-
-    var step,  box;
     var currentXCoordinate = 0;
     var currentZCoordinate = 0;
-
-    var stairRotationInDegrees = 90;
-    var verticalSupportRadius = 0.6;
-
-    var stepCount = 7;
-
+    
+    setupLight(scene);
+    createHelperAxis(scene);
+    setupPlane(scene);
+  
+    var handRailPoints = [];
     for (i = 0; i < stepCount; i++) {
         var boxGeometry = new THREE.BoxGeometry(stepDepth,stepHeight,stepWidth);
         var boxMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
-        box = new THREE.Mesh(boxGeometry, boxMaterial);
+        var box = new THREE.Mesh(boxGeometry, boxMaterial);
         box.castShadow = true;
 
         var stepSupportMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
@@ -68,7 +85,8 @@ $(function() {
         var horizontalStepSupportGeometry = new THREE.BoxGeometry(horizontalSupportLength, supportDepth, supportWidth);
         var horizontalStepSupport = new THREE.Mesh(horizontalStepSupportGeometry, stepSupportMaterial);
         horizontalStepSupport.position.x = horizontalSupportLength / 2;
-        horizontalStepSupport.position.y = -(stepHeight / 2 + supportDepth / 2);
+        var horizontalStepSupportY =  -(stepHeight / 2 + supportDepth / 2);
+        horizontalStepSupport.position.y = horizontalStepSupportY;
 
         var verticalStepSupportGeometry = new THREE.CylinderGeometry(verticalSupportRadius, verticalSupportRadius, stepHeight * 2 + spaceBetweenFootsteps);
         var verticalStepSupport = new THREE.Mesh(verticalStepSupportGeometry, stepSupportMaterial);
@@ -76,17 +94,27 @@ $(function() {
         verticalStepSupport.position.y = 1;
         verticalStepSupport.position.z = 0;
 
-        step = new THREE.Object3D();
+        var railSupportUnderStepPoint = new THREE.Vector3(0, horizontalStepSupportY, stepWidth / 2 - 1);
+        var railSupportCurve = new THREE.Vector3(0, horizontalStepSupportY, stepWidth / 2 + 1);
+        var railSupportTop = new THREE.Vector3(0, horizontalStepSupportY + railHeight, stepWidth / 2 + supportDepth / 2);
+        var railSupportPath = [railSupportUnderStepPoint, railSupportCurve, railSupportTop];
+        var railSupportCurve = new THREE.CatmullRomCurve3(railSupportPath);
+        var verticalRailSupportGeometry = new THREE.TubeGeometry(railSupportCurve, 100, supportDepth / 2, 100, false);
+        var railSupport = new THREE.Mesh(verticalRailSupportGeometry, stepSupportMaterial);
+
+        var step = new THREE.Object3D();
         step.add(box);
         step.add(horizontalStepSupport);
+        step.add(railSupport);
         if (i != stepCount - 1) {
             step.add(verticalStepSupport);
         }
 
+        var stepRotationInRadians = degreesToRadians((stepRotationInDegrees / (stepCount - 1)) * i)
+
         step.position.x = currentXCoordinate;
         step.position.z = currentZCoordinate;
         step.position.y = supportDepth + (stepHeight / 2) + i * (stepHeight + spaceBetweenFootsteps);
-        var stepRotationInRadians = degreesToRadians((stairRotationInDegrees / (stepCount - 1)) * i)
         step.rotation.y = stepRotationInRadians;
 
         scene.add(step);
@@ -95,20 +123,35 @@ $(function() {
         currentXCoordinate = currentXCoordinate + shift * Math.cos(stepRotationInRadians);
         currentZCoordinate = currentZCoordinate - shift * Math.sin(stepRotationInRadians);
     }
-  
 
-    camera.position.x = 0;
-    camera.position.y = 10;
+    
+}
+
+$(function() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = createRenderer();
+
+    const gui = new dat.GUI();
+    const parameters = {
+        stepCount: 7,
+        stairRotationInDegrees: 90
+    }
+    gui.add(parameters, 'stepCount', 2, 20)
+        .step(1)
+        .name('Step count')
+        .onChange(() => redraw(scene, parameters.stepCount, parameters.stairRotationInDegrees));
+    gui.add(parameters, 'stairRotationInDegrees', 0, 180)
+        .step(10)
+        .name('Stair rotation')
+        .onChange(() => redraw(scene, parameters.stepCount, parameters.stairRotationInDegrees));
+
+    draw(scene, parameters.stepCount, parameters.stairRotationInDegrees);
+
+    camera.position.x = -10;
+    camera.position.y = 20;
     camera.position.z = 35;
     camera.lookAt(scene.position);
-
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set( -40, 60, -10 );
-    spotLight.castShadow = true;
-    scene.add( spotLight );
-
-    var ambientLight = new THREE.AmbientLight(0x404040);
-    scene.add(ambientLight);
 
     // add the output of the renderer to the html element
     $("#WebGL-output").append(renderer.domElement);
